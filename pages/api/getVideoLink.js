55eb1784e4e7f6a7b5ff6651b2f78be21cdcdf6e
Snowy7 <snowydev7@gmail.com>
2022-10-19 14:34:24 +0300

    sad

--- pages/api/getVideoLink.js
+++ pages/api/getVideoLink.js
@@ -10,7 +10,7 @@ export default async function handler(
         if (url === undefined) {
             res.status(405).end('You must provide: a url');
         } else {
-            const browser = await puppeteer.launch({headless: true})
+            const browser = await puppeteer.launch({headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox']})
             const page = await browser.newPage()
             await page.goto(url)
 
