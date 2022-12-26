010b6b161d229fd91f287f09ad58de80bcd7c795
Snowy7 <snowydev7@gmail.com>
2022-12-26 02:14:06 +0200

    sad

---
+++ pages/api/movie/[title].js
@@ -0,0 +1,76 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import sites from "../websites.json"
+import axios from "axios";
+import cheerio from "cheerio";
+
+
+export default function handler(
+    req,
+    res
+) {
+    if (req.method === 'GET') {
+        const lang = req.query.lang || "ar";
+        if (lang === 'ar') {
+            if (req.query.title !== undefined) {
+                // Scrap the anime info from the website
+                axios.get(sites.ar.xsanime.animeInfo.url + req.query.title, {
+                    headers: {
+                        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'
+                    }
+                }).then(response => {
+                    const $ = cheerio.load(response.data);
+                    const animeInfo = {
+                        title: $(sites.ar.xsanime.animeInfo.titlePath).text(),
+                        img: $(sites.ar.xsanime.animeInfo.coverPath).attr("src"),
+                        description: $(sites.ar.xsanime.animeInfo.descriptionPath).text(),
+                        stats: [],
+                        genres: [],
+                        episodes: []
+                    };
+                    // Get the anime stats
+                    $(sites.ar.xsanime.animeInfo.statsPath).slice(1, sites.ar.xsanime.animeInfo.statsCount).each((i, el) => {
+                        animeInfo.stats.push({
+                            name: $(el).find(sites.ar.xsanime.animeInfo.statsNamePath).text(),
+                            value: $(el).text().replace($(el).find(sites.ar.xsanime.animeInfo.statsNamePath).text(), "").trim()
+                        });
+                    });
+                    $(sites.ar.xsanime.animeInfo.genrePath).each((i, el) => {
+                        animeInfo.genres.push($(el).text());
+                    });
+                    // Scrap the episodes info from the website
+                    let img;
+                    $(sites.ar.xsanime.animeInfo.episodesPath).each((i, el) => {
+                        if (i === 0) {
+                            img = animeInfo.img
+                        }
+                        const episodeNumber = $(el).find(sites.ar.xsanime.animeInfo.ep_numPath).text().replace( /^\D+/g, '')
+                        const episodeUrl = $(el).attr(sites.ar.xsanime.animeInfo.ep_urlAttr).replace(sites.ar.xsanime.episodeInfo.url, "");
+                        animeInfo.episodes.push({
+                            img,
+                            episodeNumber,
+                            episodeUrl
+                        });
+                    });
+
+                    animeInfo.episodes = animeInfo.episodes.reverse()
+
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.end(JSON.stringify(animeInfo));
+                }).catch(error => {
+                    res.statusCode = 500;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.end(JSON.stringify({
+                        error: error.message
+                    }));
+                })
+            }else{
+                res.end("No anime specified");
+            }
+        }else{
+            res.end("Invalid site");
+        }
+    }else{
+        res.status(405).end('Method not allowed');
+    }
+}
---
+++ pages/movie/[title].tsx
@@ -0,0 +1,120 @@
+import type {NextPage} from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../../styles/Anime.module.css'
+import {Navbar} from '../../components'
+import {useEffect, useState} from "react";
+import {useRouter} from "next/router";
+
+const Anime: NextPage = () => {
+    const [info, setAnimeInfo] = useState<any[] | undefined | null>(null);
+    const [episodes, setEpisodes] = useState<any[] | undefined | null>(null);
+    const router = useRouter();
+    const getAnimeData = async (title: string) => {
+        title = encodeURIComponent(title);
+        const res = await fetch(`/api/anime/${title}?site=ar`);
+        const data = await res.json();
+        if (data.error) {
+            window.location.href = "/";
+            return null;
+        }
+        setAnimeInfo(data);
+        setEpisodes(data.episodes);
+        return data;
+    }
+
+    const onSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        // filter episodes
+        // @ts-ignore
+        const filteredEpisodes = info.episodes?.filter((episode) => {
+            return episode.episodeNumber.toLowerCase().includes(e.target.value.toLowerCase());
+        });
+        setEpisodes(filteredEpisodes);
+    }
+
+    useEffect(() => {
+        console.log(window.location.pathname);
+        let title = window.location.pathname.split('/')[2];
+        if (title) {
+            getAnimeData(title);
+        }
+    }, []);
+
+
+    return (
+        <>
+            <Head>
+                {/* @ts-ignore */}
+                <title> {info && info.title} | SnowyAnime</title>
+                <meta name="description" content="Made by snowy to prove them wrong :)"/>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
+            <div className="bg"/>
+            <Navbar path={"../lLogo.svg"}/>
+            <div className={styles.container}>
+                <div className={styles.right}>
+                    <div className={styles.img_container}>
+                        {/* @ts-ignore */}
+                        <img src={info && `/api/image/${encodeURIComponent(info.img)}`} alt={info && info.title}/>
+                    </div>
+                    <div className={styles.stats_container}>
+                        {/* @ts-ignore */}
+                        {info && info.stats.map((stat, index) => (
+                            <div className={styles.stats_item} key={index}>
+                                <small>{stat.name}</small>
+                                <span>{stat.value}</span>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                <div className={styles.left}>
+                    <div className={styles.info_container}>
+                        <div className={styles.info_header}>
+                            {/* @ts-ignore */}
+                            <h1>{info && info.title}</h1>
+                        </div>
+                        <div className={styles.info_genre}>
+                            {/* @ts-ignore */}
+                            {(info && info.genres) && info.genres.map((genre, index) => (
+                                <span key={index}>{genre}{" "}</span>
+                            ))}
+                        </div>
+                        <div className={styles.info_body}>
+                            {/* @ts-ignore */}
+                            <p>{info && info.description}</p>
+                        </div>
+                    </div>
+                    <div className={styles.episodes_container}>
+                        <div className={styles.episodes_header}>
+                            <h2>Episodes</h2>
+                            <input onChange={onSearchChange} placeholder={"Enter an episode number"} type={"number"}/>
+                        </div>
+                        <div className={styles.episodes_list}>
+                        {/* @ts-ignore */}
+                        {episodes && episodes.map((episode, index) => (
+                            <div  onClick={
+                                () => {
+                                    {/* @ts-ignore */}
+                                    router.push(`/episode/${episode.episodeUrl}`);
+                                }
+                            } className={styles.episode_card} key={index}>
+                                <div className={styles.episode_card__img}  style={{backgroundImage: `url("/api/image/${encodeURIComponent(episode.img)}")`}}/>
+                                <div className={styles.episode_card__info}>
+                                    <div className={styles.episode_card__header}>
+                                        <div className={styles.episode_card__header__tag}>
+                                            <span>Episode {episode.episodeNumber}</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        ))}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Anime
